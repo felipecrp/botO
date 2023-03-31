18f41404f6e38f250b98e9d4e4a8d0228f9f24ed
Felipe Curty <felipecrp@gmail.com>
2023-03-31 20:11:06 -0300

    Initial idea

---
+++ src/message.ts
@@ -0,0 +1,45 @@
+
+class User {
+    id: string;
+    name: string;
+}
+
+class Channel {
+    id: string;
+    name: string;
+}
+
+class PrivateChannel extends Channel {
+}
+
+class Message {
+    id: string;
+    from: User;
+    in: Channel;
+    content: string;
+}
+
+/*
+on.message()
+  .from()
+  .in()
+
+export default function(on) {
+    on.message().from().in().do()
+    on.message().from().inPrivate().do()
+    on.command('^!', parser).from().in().do()
+    on.time('* * * * 3').do()
+    on.event(join|part).do()
+}
+
+listener {
+    regexp
+        in
+        from
+        message
+        
+}
+*/
+
+
+
